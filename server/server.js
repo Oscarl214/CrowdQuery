82cd086a3c183c7ee0596d3a26a042392502b4d6
Oscar Leal <oscarleal234@gmail.com>
2023-09-30 17:41:16 -0500

    added a form view

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
+const cors = require('cors');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -14,6 +14,13 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+app.use(
+  cors({
+    origin: [],
+    methods: ['POST', 'DELETE'],
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
